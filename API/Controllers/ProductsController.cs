b880954707121d242a50e11b773268f4e0084eaa
AnassEREKYSY <ereanass@gmail.com>
2024-09-21 17:02:35 +0200

    Implementing GenericRepository Pattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,24 +9,24 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController(IProductRepository repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type , string? sort){
-        return  Ok(await repo.GetProductsAsync(brand,type,sort));
+        return  Ok(await repo.ListAllAsync());
     }
 
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id){
-        var product = await repo.GetProductByIdAsync(id);
+        var product = await repo.GetByIdAsync(id);
         if(product == null) return NotFound();
         return product;
     }
 
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product){
-        repo.AddProduct(product);
-        if(await repo.SaveChangesAsync()){
+        repo.Add(product);
+        if(await repo.SaveAllAsync()){
             return CreatedAtAction("GetProduct", new {id=product.Id},product);
         }
         return BadRequest("Problem Creating product");
@@ -39,8 +39,8 @@ public class ProductsController(IProductRepository repo) : ControllerBase
             return BadRequest("Cannot Update the product");
         }
 
-        repo.UpdateProduct(product);
-        if(await repo.SaveChangesAsync()){
+        repo.Update(product);
+        if(await repo.SaveAllAsync()){
             return NoContent();
         }
         return BadRequest("Problem Updating product");
@@ -49,12 +49,12 @@ public class ProductsController(IProductRepository repo) : ControllerBase
     [HttpDelete]
     public async Task<ActionResult> DeleteProduct(int id){
 
-        var product=await repo.GetProductByIdAsync(id);
+        var product=await repo.GetByIdAsync(id);
 
         if(product == null)return NotFound();
 
-        repo.DeleteProduct(product);
-        if(await repo.SaveChangesAsync()){
+        repo.Remove(product);
+        if(await repo.SaveAllAsync()){
             return NoContent();
         }
         return BadRequest("Problem Deleting product");
@@ -62,15 +62,15 @@ public class ProductsController(IProductRepository repo) : ControllerBase
     
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetBrands(int id){
-        return Ok(await repo.GetBrandsAsync());
+        return Ok();
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetTypes(int id){
-        return Ok(await repo.GetTypesAsync());
+        return Ok();
     }
 
     private bool ProductExists(int id){
-        return repo.ProductExists(id);
+        return repo.Exists(id);
     }
 }
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03d3c25322bbb73c24242cd8821a077254c858b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbaf2e2b07b4c6a4d58fbc565c0662ce6be20f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
