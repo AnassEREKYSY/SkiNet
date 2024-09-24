50f6c366749184743d89916e04e6547467f9ddb5
AnassEREKYSY <ereanass@gmail.com>
2024-09-24 18:43:45 +0200

    Adding Cors and Erros Handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.MiddleWare;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,13 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
 });
 builder.Services.AddScoped<IProductRepository, ProdcutRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleWare>();
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840a56bdb2ce64f5c6c02ffed3b9312355ab636b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555a1d38c30e99036b9c6df453f4bd62622db9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
