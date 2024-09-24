e74095d45b78e8db9ea08b548307917147766d2b
AnassEREKYSY <ereanass@gmail.com>
2024-09-24 15:46:51 +0200

    adding Base Controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fe4212d6e0d18168916a6182ffcfde6809f773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840a56bdb2ce64f5c6c02ffed3b9312355ab636b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
