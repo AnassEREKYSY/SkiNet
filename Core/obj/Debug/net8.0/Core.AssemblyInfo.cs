124eec43c17a2d5c8186ebcc23906fff36134792
AnassEREKYSY <ereanass@gmail.com>
2024-09-25 15:32:39 +0200

    setting up the Page Header (navbar)

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f6c366749184743d89916e04e6547467f9ddb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab845788cecdeda369d8976cc3f81bcfbbacd88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
