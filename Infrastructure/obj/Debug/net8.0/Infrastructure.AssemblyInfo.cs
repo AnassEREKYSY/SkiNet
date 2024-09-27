94bf387ab793462be0d8e5c5c00b0d3bdd0c091a
AnassEREKYSY <ereanass@gmail.com>
2024-09-27 20:29:01 +0200

    Debugging the Filter feature

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bee295bca7189fc2753eddd849959c573294d59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ba460c560d3617a129f12bee1aea4b9c710c47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
